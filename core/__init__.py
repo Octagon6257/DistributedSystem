c7ca19aa868051af461d3754cfcf26900a011ce1
Octagon6257 <l.mazzoli7@campus.uniurb.it>
2026-02-01 11:21:33 +0100

    Modified __init__.py to avoid circular dependencies

--- core/__init__.py
+++ core/__init__.py
@@ -1,13 +1,33 @@
-from .ChordNode import ChordNode
-from .FingerTable import FingerTable
-from .TopologyManager import TopologyManager
-from .DataStore import DataStore
-from .DataTransferManager import DataTransferManager
-
 __all__ = [
     'ChordNode',
     'FingerTable',
     'TopologyManager',
     'DataStore',
-    'DataTransferManager'
+    'DataTransferManager',
+    'NodeRef',
+    'RemoteNode'
 ]
+
+def __getattr__(name):
+    if name == 'ChordNode':
+        from .ChordNode import ChordNode
+        return ChordNode
+    elif name == 'FingerTable':
+        from .FingerTable import FingerTable
+        return FingerTable
+    elif name == 'TopologyManager':
+        from .TopologyManager import TopologyManager
+        return TopologyManager
+    elif name == 'DataStore':
+        from .DataStore import DataStore
+        return DataStore
+    elif name == 'DataTransferManager':
+        from .DataTransferManager import DataTransferManager
+        return DataTransferManager
+    elif name == 'NodeRef':
+        from .NodeRef import NodeRef
+        return NodeRef
+    elif name == 'RemoteNode':
+        from .NodeRef import RemoteNode
+        return RemoteNode
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
